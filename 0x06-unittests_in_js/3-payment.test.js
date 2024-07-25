cebeb8f4683bb19c6fa028c663f1c519151c836b
rafiki1990 <blaiserafiki84@gmail.com>
2024-07-25 16:29:46 +0200

    3-payment.js

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,23 @@
+// 3-payment.test.js
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberSpy;
+
+  beforeEach(() => {
+    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  });
+
+  afterEach(() => {
+    calculateNumberSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    sendPaymentRequestToApi(100, 20);
+    
+    expect(calculateNumberSpy.calledOnceWith('SUM', 100, 20)).to.be.true;
+  });
+});
