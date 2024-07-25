bec519050c4bf25daaecfb2ae6a960f4c0583159
rafiki1990 <blaiserafiki84@gmail.com>
2024-07-25 15:46:57 +0200

    1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,21 +1,20 @@
 // 1-calcul.js
 function calculateNumber(type, a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
 
-  switch (type) {
-    case 'SUM':
-      return roundedA + roundedB;
-    case 'SUBTRACT':
-      return roundedA - roundedB;
-    case 'DIVIDE':
-      if (roundedB === 0) {
-        return 'Error';
-      }
-      return roundedA / roundedB;
-    default:
-      throw new Error('Invalid type');
-  }
+    if (type === 'SUM') {
+        return roundedA + roundedB;
+    } else if (type === 'SUBTRACT') {
+        return roundedA - roundedB;
+    } else if (type === 'DIVIDE') {
+        if (roundedB === 0) {
+            return 'Error';
+        }
+        return roundedA / roundedB;
+    } else {
+        throw new Error('Invalid type');
+    }
 }
 
 module.exports = calculateNumber;
